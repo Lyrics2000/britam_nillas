16beb853263fc758a9e137380385e6248d2a0982
Achongo A. Thomas <tachongo@BRITAMGROUP.COM>
2025-01-21 10:35:03 +0300

    amm123

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    "corsheaders",
     'home',
     'logs'
    
@@ -96,7 +97,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+CORS_ALLOW_CREDENTIALS = True
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
